e2ea1bee75fa63e4a667b170d1969f2a9877a42e
bufgix <ooruc471@yandex.com>
2020-09-02 17:28:10 +0300

    fix

--- routes/kurumsal.js
+++ routes/kurumsal.js
@@ -2,15 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/iptal", (_, res) => {
-  res.render("info/cancel");
+  res.render("info/cancel", { title: "İptal Şartları" });
 });
 
 router.get("/kargo", (_, res) => {
-  res.render("info/cargo");
+  res.render("info/cargo", { title: "Kargo" });
 });
 
 router.get("/gizlilik", (_, res) => {
-  res.render("info/privacy");
+  res.render("info/privacy", {title: "Gizlilik"});
 });
 
 module.exports = router;
